e454f3962eb94bf6dee5bef15fd26d10ac3eab0e
Rxann <80224521+Rxann@users.noreply.github.com>
2022-04-06 18:28:11 -0400

    Add jsdoc

--- os.js
+++ os.js
@@ -1,5 +1,5 @@
 const os = require("os");
-
+/**UUID Class */
 class OS {
   constructor() {
     this.platform = os.platform();
@@ -15,56 +15,67 @@ class OS {
     this.EOL = os.EOL;
     this.uptime = os.uptime();
   }
+  /**Get the platform of the OS*/
   async platform() {
     return new Promise((resolve, reject) => {
       resolve(this.platform);
     });
   }
+  /**Get the arch of the OS */
   async arch() {
     return new Promise((resolve, reject) => {
       resolve(this.arch);
     });
   }
+  /**Get the OS Release */
   async release() {
     return new Promise((resolve, reject) => {
       resolve(this.release);
     });
   }
+  /**Get the type of OS */
   async type() {
     return new Promise((resolve, reject) => {
       resolve(this.type);
     });
   }
+  /**Get the hostname of the computer */
   async hostname() {
     return new Promise((resolve, reject) => {
       resolve(this.hostname);
     });
   }
+  /**Get the average load time of the OS */
   async loadavg() {
     return new Promise((resolve, reject) => {
       resolve(this.loadavg);
     });
   }
+  /**Get the total memory on the system */
   async totalmem() {
     return new Promise((resolve, reject) => {
       resolve(this.totalmem);
     });
   }
+  /**Get the free memory on the system */
   async freemem() {
     return new Promise((resolve, reject) => {
       resolve(this.freemem);
     });
   }
+  /**Get the CPU's and their details*/
   async cpus() {
     return new Promise((resolve, reject) => {
       resolve(this.cpus);
     });
   }
+  /**Get the network Interfaces of the system */
   async networkInterfaces() {
     return new Promise((resolve, reject) => {
       resolve(this.networkInterfaces);
     });
   }
+  /**Get the system uptime */
   async uptime() {
     return new Promise((resolve, reject) => {
       resolve(this.uptime);
